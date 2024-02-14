10257a77ab17233a38d9353d2bc07c07f8dd786e
Tom Hubrecht <tom@hubrecht.ovh>
2024-02-14 15:40:53 +0100

    chore(nix): Reformat

--- default.nix
+++ default.nix
@@ -1,14 +1,20 @@
-{ sources ? import ./npins, pkgs ? import sources.nixpkgs { } }:
+{
+  sources ? import ./npins,
+  pkgs ? import sources.nixpkgs { },
+}:
+
 {
   shell = pkgs.mkShell {
-    buildInputs = with pkgs; [
-      (python3.withPackages (ps: [
-        ps.fastapi
-        ps.uvicorn
-        ps.sqlalchemy
-        ps.pydantic
-      ]))
-      jq
+    buildInputs = [
+      (pkgs.python3.withPackages (
+        ps: [
+          ps.fastapi
+          ps.uvicorn
+          ps.sqlalchemy
+          ps.pydantic
+        ]
+      ))
+      pkgs.jq
     ];
   };
 }
