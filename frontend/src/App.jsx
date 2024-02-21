8a3af0cd9bf81e3659d16fbea74423fbe37c2aa8
Raito Bezarius <masterancpp@gmail.com>
2024-02-22 00:19:36 +0100

    feat(front): very simple homepage
    Shows all machines in the current system.
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 import { Notification } from "./components/Notification";
 import { Machine } from "./models/Machine";
 
@@ -6,28 +6,20 @@ const fetchMachines = async () =>
   (await fetch("http://127.0.0.1:8000/machines")).json();
 
 function App() {
-  const [state, setState] = createSignal("Loading...");
   const [machines, setMachines] = createSignal([]);
 
-  function getData() {
-    setTimeout(() => {
-      setState("Here are some values: Value 1, Value 2, Value 3");
-      setMachines(fetchMachines())
-    }, 1000); // Simulating an asynchronous operation
-  }
+  createEffect(() => {
+    fetchMachines().then(machines => {
+      setMachines(machines);
+    });
+  })
 
   return (
     <>
       <div>
-        <For each={machines}>
-          {(machine, i) => <Machine machine={machine} />} i
+        <For each={machines()}>
+          {machine => <Machine machine={machine} />}
         </For>
-
-        <button class="button is-primary mb-3" onclick={getData}>
-          Get Values
-        </button>
-
-        <Notification state={state} />
       </div>
     </>
   );
--- frontend/src/models/Machine.jsx
+++ frontend/src/models/Machine.jsx
@@ -1,5 +1,7 @@
 export function Machine(props) {
-  return <>
-    <div class="box">props.machine</div>
-  </>;
+  return (
+    <div class="box">
+      <h3><b>{props.machine.identifier}</b></h3>
+    </div>
+  );
 }
