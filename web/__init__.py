f44af7b04ed8005d9ab5cdd7323f9d0bc0149845
Tom Hubrecht <tom@hubrecht.ovh>
2024-02-27 18:01:35 +0100

    feat(web): Add arbitrary diff, and ensure that the compared deployments are for the same machine

--- web/__init__.py
+++ web/__init__.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import Depends, FastAPI, HTTPException, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
@@ -44,6 +46,49 @@ def get_or_404(session, model, pk):
     return o
 
 
+def size_from_path(path: str, db: Session):
+    return db.query(S).where(S.path == path).one().closure_size
+
+
+def get_diff(current: D, previous: Optional[D], db: Session):
+    if previous is not None and current.target_machine_id != previous.target_machine_id:
+        raise HTTPException(
+            status_code=400, detail="The deployments come from two different machines"
+        )
+
+    current_pkgs = closure_paths_to_map(current.closure)
+    current_size = size_from_path(current.toplevel, db)
+
+    if previous is None:
+        previous_pkgs = dict()
+        previous_size = 0
+    else:
+        previous_pkgs = closure_paths_to_map(previous.closure)
+        previous_size = size_from_path(previous.toplevel, db)
+
+    current_pnames = set(current_pkgs.keys())
+    previous_pnames = set(previous_pkgs.keys())
+
+    common = current_pnames & previous_pnames
+    added = current_pnames - previous_pnames
+    removed = previous_pnames - current_pnames
+
+    # Announce version changes.
+    changed = set()
+    for pname in common:
+        if current_pkgs[pname] != previous_pkgs[pname]:
+            changed.add(pname)
+
+    return {
+        "changed": {
+            p: {"old": previous_pkgs[p], "new": current_pkgs[p]} for p in changed
+        },
+        "removed": {p: previous_pkgs[p] for p in removed},
+        "added": {p: current_pkgs[p] for p in added},
+        "sizes": {"old": previous_size, "new": current_size},
+    }
+
+
 @app.get("/machines")
 def get_machines(db: Session = Depends(get_db)):
     return db.query(M).all()
@@ -73,73 +118,18 @@ def get_deployments(machine_identifier: str, db: Session = Depends(get_db)):
 
 
 @app.get("/diff-latest")
-def compare_with_previous(deployment_id: int, db: Session = Depends(get_db)):
-    def size_from_path(path: str):
-        return db.query(S).where(S.path == path).one().closure_size
-
+def diff_latest(deployment_id: int, db: Session = Depends(get_db)):
     current = get_or_404(db, D, deployment_id)
     previous = db.query(D).where(D.id < deployment_id).order_by(D.id.desc()).first()
 
-    current_pkgs = closure_paths_to_map(current.closure)
-    current_size = size_from_path(current.toplevel)
-
-    if previous is None:
-        previous_pkgs = dict()
-        previous_size = 0
-    else:
-        previous_pkgs = closure_paths_to_map(previous.closure)
-        previous_size = size_from_path(previous.toplevel)
-
-    current_pnames = set(current_pkgs.keys())
-    previous_pnames = set(previous_pkgs.keys())
-
-    common = current_pnames & previous_pnames
-    added = current_pnames - previous_pnames
-    removed = previous_pnames - current_pnames
-
-    # Announce version changes.
-    changed = set()
-    for pname in common:
-        if current_pkgs[pname] != previous_pkgs[pname]:
-            changed.add(pname)
-
-    return {
-        "changed": {
-            p: {"old": previous_pkgs[p], "new": current_pkgs[p]} for p in changed
-        },
-        "removed": {p: previous_pkgs[p] for p in removed},
-        "added": {p: current_pkgs[p] for p in added},
-        "sizes": {"old": previous_size, "new": current_size},
-    }
+    return get_diff(current, previous, db)
 
 
-#
-#
-# @app.get("/diff/{machine_identifier}")
-# def compare_deployments(
-#     left_deployment_id: int, right_deployment_id: int, db: Session = Depends(get_db)
-# ):
-#     left = get_or_404(db, models.Deployment, left_deployment_id)
-#     right = get_or_404(db, models.Deployment, right_deployment_id)
-#
-#     left_closure_map = closure_paths_to_map(left.closure)
-#     right_closure_map = closure_paths_to_map(right.closure)
-#
-#     left_package_names = set(left_closure_map.keys())
-#     right_package_names = set(right_closure_map.keys())
-#
-#     common_package_names = sorted(left_package_names & right_package_names)
-#     left_only_package_names = sorted(left_package_names - right_package_names)
-#     right_only_package_names = sorted(right_package_names - left_package_names)
-#
-#     # Announce version changes.
-#     package_names_with_changed_versions = []
-#     for pname in common_package_names:
-#         if left_closure_map[pname] != right_closure_map[pname]:
-#             package_names_with_changed_versions.append(pname)
-#
-#     return {
-#         "changed": package_names_with_changed_versions,
-#         "removed": left_only_package_names,
-#         "added": right_only_package_names,
-#     }
+@app.get("/diff")
+def compare_deployments(
+    left_deployment_id: int, right_deployment_id: int, db: Session = Depends(get_db)
+):
+    current = get_or_404(db, D, left_deployment_id)
+    previous = get_or_404(db, D, right_deployment_id)
+
+    return get_diff(current, previous, db)
