886e9d6deb4a51da50b2e024db7f82def8ee9801
sinavir <sinavir@sinavir.fr>
2024-02-28 01:58:04 +0100

    feat(api): Make db url customizable

--- src/api/db.py
+++ src/api/db.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mimir.db"
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL", "sqlite:///./mimir.db")
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
