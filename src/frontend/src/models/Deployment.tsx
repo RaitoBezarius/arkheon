7c46cd5c8a612f86c63cc4929b038f00456d8e04
Tom Hubrecht <tom@hubrecht.ovh>
2024-03-01 10:10:55 +0100

    feat(frontend): tweaks again

--- src/frontend/src/models/Deployment.tsx
+++ src/frontend/src/models/Deployment.tsx
@@ -4,20 +4,20 @@ import { date } from "../utils";
 export const Deployment: Component<Deployment> = (props) => {
   return (
     <tr>
-      <td>
+      <td class="date">
         <span class="is-family-monospace">{date(props.created_at)}</span>
-      </td>
-
-      <td>
-        <a class="is-family-monospace" href={`/diff/${props.id}`}>
-          {props.toplevel}
-        </a>
       </td>
 
       <td>
         <span>
           by&nbsp;<b>{props.operator_id}</b>
         </span>
+      </td>
+
+      <td>
+        <a class="is-family-monospace" href={`/diff/${props.id}`}>
+          {props.toplevel}
+        </a>
       </td>
     </tr>
   );
