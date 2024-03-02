a8f099a28593d578036f9fda4dab4ec6b1ef7681
Tom Hubrecht <tom@hubrecht.ovh>
2024-03-02 17:50:34 +0100

    chore(db): arkheon

--- src/api/db.py
+++ src/api/db.py
@@ -4,8 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy.types import BigInteger
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL", "sqlite:///./mimir.db")
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "SQLALCHEMY_DATABASE_URL", "sqlite:///./arkheon.db"
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
