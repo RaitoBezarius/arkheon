9ed6936a597e8726bd584f747d74a1d3a0c080ed
Tom Hubrecht <tom@hubrecht.ovh>
2024-03-06 09:42:15 +0100

    chore(module): Use inherit

--- module.nix
+++ module.nix
@@ -5,17 +5,40 @@
   modulesPath,
   ...
 }:
+
 let
+  inherit (lib)
+    literalExpression
+    mkEnableOption
+    mkOption
+    mkIf
+    mkDefault
+    optional
+    optionalString
+    ;
+
+  inherit (lib.types)
+    attrsOf
+    nullOr
+    package
+    path
+    port
+    str
+    submodule
+    ;
+
   cfg = config.services.arkheon;
   hasSSL = with cfg.nginx; onlySSL || enableSSL || addSSL || forceSSL;
 in
+
 {
   options.services.arkheon = {
-    enable = lib.mkEnableOption "Arkheon";
-    pythonEnv = lib.mkOption {
+    enable = mkEnableOption "Arkheon";
+
+    pythonEnv = mkOption {
       internal = true;
       visible = false;
-      type = lib.types.package;
+      type = package;
       default = pkgs.python3.withPackages (
         ps: [
           ps.arkheon
@@ -23,7 +46,8 @@ in
           ps.psycopg2
         ]
       );
-      example = lib.literalExpression ''
+
+      example = literalExpression ''
         pkgs.python3.withPackages (ps: [
           ps.arkheon
           ps.uvicorn
@@ -31,41 +55,47 @@ in
         ]);
       '';
     };
-    port = lib.mkOption {
-      type = with lib.types; nullOr port;
+
+    port = mkOption {
+      type = nullOr port;
       default = 8007;
       description = "Port of the server (will be passed using --bind flag)";
     };
-    address = lib.mkOption {
-      type = lib.types.str;
+
+    address = mkOption {
+      type = str;
       default = "127.0.0.1";
       description = "The address the server will listen on (will be passed using --host flag)";
     };
-    settings = lib.mkOption {
+
+    settings = mkOption {
       type =
-        with lib.types;
+
         submodule {
           freeformType = attrsOf str;
-          options.SQLALCHEMY_DATABASE_URL = lib.mkOption { description = "Database url"; };
+          options.SQLALCHEMY_DATABASE_URL = mkOption { description = "Database url"; };
         };
       description = "Settings to pass as environment variables";
     };
-    envFile = lib.mkOption {
-      type = with lib.types; nullOr path;
+
+    envFile = mkOption {
+      type = nullOr path;
       description = "Environment file to append to settings.";
       default = null;
     };
-    domain = lib.mkOption {
-      type = lib.types.str;
+
+    domain = mkOption {
+      type = str;
       description = "Hostname for reverse proxy config. To configure";
     };
-    nginx = lib.mkOption {
-      type = lib.types.nullOr (
-        lib.types.submodule (
+
+    nginx = mkOption {
+      type = nullOr (
+        submodule (
           import (modulesPath + "/services/web-servers/nginx/vhost-options.nix") { inherit config lib; }
         )
       );
-      example = lib.literalExpression ''
+      example = literalExpression ''
         {
           serverAliases = [
             "arkheon.''${config.networking.domain}"
@@ -75,7 +105,7 @@ in
           enableACME = true;
         }
       '';
-      description = lib.mdDoc ''
+      description = ''
         With this option, you can customize an nginx virtual host which already
         has sensible defaults for Arkheon. If enabled, then by default, the
         serverName is ''${domain}, If this is set to null, no nginx virtualHost
@@ -86,7 +116,7 @@ in
 
   config = {
     services = {
-      postgresql = lib.mkIf (lib.hasPrefix "postgresql" cfg.settings.SQLALCHEMY_DATABASE_URL) {
+      postgresql = mkIf (lib.hasPrefix "postgresql" cfg.settings.SQLALCHEMY_DATABASE_URL) {
         enable = true;
         ensureUsers = [
           {
@@ -98,11 +128,12 @@ in
       };
 
       arkheon = {
-        settings.SQLALCHEMY_DATABASE_URL = lib.mkDefault "postgresql+psycopg2:///arkheon?host=/run/postgresql";
+        settings.SQLALCHEMY_DATABASE_URL = mkDefault "postgresql+psycopg2:///arkheon?host=/run/postgresql";
+
         nginx.locations = {
           "/" = {
             root = pkgs.python3.pkgs.arkheon.frontend.override {
-              backendUrl = "http${lib.optionalString hasSSL "s"}://${cfg.domain}/api";
+              backendUrl = "http${optionalString hasSSL "s"}://${cfg.domain}/api";
             };
             tryFiles = "$uri /index.html";
           };
@@ -110,20 +141,20 @@ in
         };
       };
 
-      nginx = lib.mkIf (cfg.nginx != null) {
+      nginx = mkIf (cfg.nginx != null) {
         enable = true;
         virtualHosts.${cfg.domain} = cfg.nginx;
       };
     };
 
-    systemd.services.arkheon = lib.mkIf cfg.enable {
+    systemd.services.arkheon = mkIf cfg.enable {
       environment = cfg.settings;
       path = [ cfg.pythonEnv ];
       script = "daphne arkheon:app -b ${cfg.address} -p ${builtins.toString cfg.port}";
       serviceConfig = {
         User = "arkheon";
         DynamicUser = true;
-        EnvironmentFile = lib.optional (cfg.envFile != null) cfg.envFile;
+        EnvironmentFile = optional (cfg.envFile != null) cfg.envFile;
       };
       wantedBy = [ "multi-user.target" ];
       wants = [ "postgresql.target" ];
