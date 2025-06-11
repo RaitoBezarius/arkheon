cc6bc98f3fc09cfb55c285461908ed305ce33954
Tom Hubrecht <tom@hubrecht.ovh>
2025-06-11 15:55:44 +0200

    feat(config): Get rid of pydantic-settings
    LoadCredential is better suited to our needs

--- src/api/config.py
+++ src/api/config.py
@@ -1,10 +1,10 @@
 import logging
 from contextlib import asynccontextmanager
-from typing import Annotated, List, Optional
+from typing import List
 
 from fastapi import FastAPI
-from pydantic import BaseModel, Field, HttpUrl, TypeAdapter
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from loadcredential import Credentials
+from pydantic import BaseModel, HttpUrl, TypeAdapter
 
 from .db import SessionLocal
 from .models import Machine as M
@@ -13,36 +13,32 @@ from .models import WebHook as W
 logger = logging.getLogger(__name__)
 
 
+credentials = Credentials(env_prefix="ARKHEON_")
+
+
 class WebHookConfig(BaseModel):
     machine: str
     endpoint: HttpUrl
 
 
-class Settings(BaseSettings):
-    token: Annotated[Optional[str], Field] = None
-    webhook_file: Annotated[Optional[str], Field] = None
-
-    model_config = SettingsConfigDict(env_prefix="ARKHEON_")
-
-
-settings = Settings()
-
-
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    if settings.token is None:
-        logger.warn("No token is provided for Arkheon.")
+    token = credentials.get("TOKEN")
+    webhook_file = credentials.get("WEBHOOK_FILE")
 
-    if settings.webhook_file is None:
-        logger.warn(
+    if token is None:
+        logger.warning("No token is provided for Arkheon.")
+
+    if webhook_file is None:
+        logger.warning(
             "No webhook file declared, notifications on records will be disabled."
         )
     else:
         # Read the webhook file and store the data in the DB
-        with open(settings.webhook_file) as fd:
+        with open(webhook_file) as fd:
             ws = TypeAdapter(List[WebHookConfig]).validate_json(fd.read())
 
-        logger.info(f"Registering webhooks from {settings.webhook_file}.")
+        logger.info(f"Registering webhooks from {webhook_file}.")
         with SessionLocal() as db:
             for w in ws:
                 trigger = db.query(M).filter(M.identifier == w.machine).one()
