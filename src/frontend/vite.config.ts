367df118e928b1c1158b65af52136ace2050b656
Tom Hubrecht <tom@hubrecht.ovh>
2025-09-14 19:46:15 +0200

    feat(viteconfig): Proxy the backend

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
+  server: {
+    open: "/",
+    proxy: {
+      "/api/": "http://localhost:8000/",
+    },
+  },
   plugins: [solid()],
 });
