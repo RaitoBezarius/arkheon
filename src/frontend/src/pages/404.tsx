d2cf5577e3a51252a96437b8624e386a94b7b52d
Tom Hubrecht <tom@hubrecht.ovh>
2025-09-15 16:20:42 +0200

    chore(frontend/404): Refactor

--- src/frontend/src/pages/404.tsx
+++ src/frontend/src/pages/404.tsx
@@ -2,13 +2,13 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-export default function NotFound() {
+const NotFound = () => {
   return (
-    <>
-      <div class="notification is-warning has-text-centered px-3 py-5">
-        <h1 class="title">Page not found...</h1>
-        <a href="/">Go home</a>
-      </div>
-    </>
+    <div class="notification is-warning has-text-centered px-3 py-5">
+      <h1 class="title">Page not found...</h1>
+      <a href="/">Go home</a>
+    </div>
   );
-}
+};
+
+export default NotFound;
